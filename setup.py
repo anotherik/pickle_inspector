fe9a18b3eab9df5df6f84a8b42ac17052f6c79b9
Ricardo <ricardojotagoncalves@hotmail.com>
2025-08-06 18:01:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 import shutil
 import sys
+import os
+
+# Read the README file for long description
+def read_readme():
+    with open("README.md", "r", encoding="utf-8") as fh:
+        return fh.read()
 
 # Check for 2to3 availability
 if not shutil.which("2to3"):
@@ -8,10 +14,45 @@ if not shutil.which("2to3"):
 
 setup(
     name='pickle_inspector',
-    version='0.1',
+    version='0.1.0',
     description='Static analysis tool to detect insecure deserialization in Python code',
-    author='Your Name',
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    author='anotherik',
+    author_email='anotherik@hotmail.com',
+    url='https://github.com/anotherik/pickle_inspector',
+    project_urls={
+        'Bug Reports': 'https://github.com/anotherik/pickle_inspector/issues',
+        'Source': 'https://github.com/anotherik/pickle_inspector',
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Security Engineers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Security',
+        'Topic :: Software Development :: Testing',
+    ],
+    keywords='security static-analysis pickle deserialization vulnerability',
     packages=find_packages(),
+    py_modules=[
+        'cli',
+        'analyzer', 
+        'ast_parser',
+        'indexer',
+        'resolver',
+        'utils',
+        'report',
+        'sources_and_sinks'
+    ],
     install_requires=[
         'tqdm>=4.64.0',
         'rich>=13.3.0',
@@ -19,8 +60,10 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'pickle-inspector = cli:main'
-        ]
+            'pickle-inspector=cli:main',
+        ],
     },
     python_requires='>=3.7',
+    include_package_data=True,
+    zip_safe=False,
 )
